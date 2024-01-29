aaf6f220857da3c56bf438bc6956cd38b449bd9d
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2024-01-29 13:04:15 +0000

    Set up files and db

---
+++ mysql_start_script.sql
@@ -0,0 +1,14 @@
+-- Create a new database
+CREATE DATABASE IF NOT EXISTS Student_Attendance_System;
+
+-- Switch to the newly created database
+USE Student_Attendance_System;
+
+-- Create a new user
+CREATE USER 'salun'@'localhost' IDENTIFIED BY 'salun';
+
+-- Grant privileges to the user on the database
+GRANT ALL PRIVILEGES ON Student_Attendance_System.* TO 'salun'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
