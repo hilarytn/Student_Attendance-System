848b589716504fb30443db78548b06e3ae65f1ce
Hilary Titus Naor <hilarytn@gmail.com>
2024-02-04 13:11:26 +0100

    Add db PORT

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: 3306
 });
 
 // Create tables if not exist
