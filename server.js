1a9976763f9d93f8d09f99ea2d29ae81cf32ec50
Hilary Titus Naor <hilarytn@gmail.com>
2024-02-04 12:57:09 +0100

    Fix database env variables

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Create tables if not exist
