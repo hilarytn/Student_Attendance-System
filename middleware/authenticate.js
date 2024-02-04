7020aa80f9a1102cc4d18dc1c6f947dd24104221
Hilary Titus Naor <hilarytn@gmail.com>
2024-02-04 15:48:38 +0100

    Add authenticate middleware logic

---
+++ middleware/authenticate.js
@@ -0,0 +1,22 @@
+import  jwt  from "jsonwebtoken";
+
+export const authenticate = async(req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        if(!token) {
+            return res.status(401).json({message: "Access denied. No token provided"});
+        }
+        const decoded = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        req.user = decoded;
+        next();
+        
+    } catch (error) {
+        if(error.name === 'TokenExpiredError') {
+            return res.status(401).json({error: error.message});   
+        }
+        if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ message: "Invalid token" });
+        }
+        res.status(500).json({ message: "Internal server error" });  
+    }
+}
