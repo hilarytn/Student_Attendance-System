2fe3ec089d839b27e7afde7912c6086d0faa54ee
Hilary Titus Naor <hilarytn@gmail.com>
2024-02-04 12:08:37 +0100

    Fix sql server script to not select database yet

--- script.sql
+++ script.sql
@@ -1,11 +1,8 @@
 -- Create a new database
 CREATE DATABASE IF NOT EXISTS Student_Attendance_System;
 
--- Switch to the newly created database
-USE Student_Attendance_System;
-
 -- Create a new user
-CREATE USER 'salun'@'localhost' IDENTIFIED BY 'sa
+CREATE USER 'salun'@'localhost' IDENTIFIED BY 'salun';
 
 -- Grant privileges to the user on the database
 GRANT ALL PRIVILEGES ON Student_Attendance_System.* TO 'salun'@'localhost';
