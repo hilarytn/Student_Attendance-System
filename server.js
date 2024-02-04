b530ac0d8ffd7992eb07f8c8e30c703c470a831a
Hilary Titus Naor <hilarytn@gmail.com>
2024-02-04 12:53:31 +0100

    Fetch db_host from the environment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000;
 app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
-  host: HOST,
+  host: process.env.DB_HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
