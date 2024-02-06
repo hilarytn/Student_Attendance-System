1509036e304f99fe820b3a974b2255a1fb8dbe5d
Hilary Titus Naor <hilarytn@gmail.com>
2024-02-06 13:40:05 +0100

    Move add-student logic to controller

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -1,1 +1,35 @@
-student
+const connection = require('../models/db');
+
+
+const addStudent = async (req, res) => {
+    const { regNumber, studentName } = req.body;
+
+    // Check if regNumber already exists
+    const checkQuery = `SELECT * FROM students WHERE reg_number = '${regNumber}'`;
+
+    connection.query(checkQuery, (checkError, checkResults) => {
+      if (checkError) {
+        throw checkError;
+      }
+  
+      // If regNumber already exists, return a message
+      if (checkResults.length > 0) {
+        return res.status(400).json({ message: 'Registration number already exists' });
+      }
+      if (regNumber || studentName.length === 0) {
+        return res.status(400).json({ message: 'Registration Number or Student Name cannot be blank' })
+      } 
+      // If regNumber does not exist, insert the new student
+      const insertQuery = `INSERT INTO students (reg_number, student_name) VALUES ('${regNumber}', '${studentName}')`;
+  
+      connection.query(insertQuery, (insertError, insertResults) => {
+        if (insertError) {
+          throw insertError;
+        }
+  
+        res.json({ message: 'Student added successfully' });
+      });
+    });
+}
+
+module.exports = { addStudent };
