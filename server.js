a5c5a7429983b539df1cf3f02dba58332af7d0e1
Hilary Titus Naor <hilarytn@gmail.com>
2024-02-06 14:33:12 +0100

    Add url and path modules

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ import express from 'express';
 import mysql from 'mysql2';
 import ip from'ip';
 import dotenv from'dotenv';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import { addStudent } from './controllers/StudentController.js';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app =  express();
 
@@ -15,9 +19,8 @@ const PORT = process.env.PORT || 4000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
+app.set('views', join(__dirname, 'views'));
+app.use('/assets', express.static(join(__dirname, 'assets')));
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
