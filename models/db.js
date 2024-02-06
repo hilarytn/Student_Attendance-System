b5392f9b772ba25a9171da08dacbe50f2a103023
Hilary Titus Naor <hilarytn@gmail.com>
2024-02-06 19:47:33 +0100

    Add admin table

--- models/db.js
+++ models/db.js
@@ -13,6 +13,15 @@ export const connection = mysql.createConnection({
 
 // Create tables if not exist
 connection.query(`
+  CREATE TABLE IF NOT EXISTS admins (
+    admin_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+  );
+`)
+
+// Create tables if not exist
+connection.query(`
   CREATE TABLE IF NOT EXISTS students (
     student_id INT AUTO_INCREMENT PRIMARY KEY,
     reg_number VARCHAR(255),
